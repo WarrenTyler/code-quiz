2d9cae79a69e4ba49eb72df31d5721a67240dcd5
WarrenT <warrentyler1024@gmail.com>
2023-01-17 01:54:08 +0000

    refactor name choice

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,20 +1,27 @@
-const startButton = document.querySelector('#start');
-const questionDiv = document.querySelector('#questions');
-const startScreen = document.querySelector('#start-screen');
-const questionTitle = document.querySelector('#question-title');
-const choicesDiv = document.querySelector('#choices');
-
+const startButton = document.querySelector("#start");
+const questionDiv = document.querySelector("#questions");
+const startScreen = document.querySelector("#start-screen");
+const questionTitle = document.querySelector("#question-title");
+const choicesDiv = document.querySelector("#choices");
 
 const questions = [
-  {question: "quest-1", answers:["q1-an1", "q1-an2", "q1-an3", "q1-an4"], correctAnswer: 3},
-  {question: "quest-2", answers:["q2-an1", "q2-an2", "q2-an3", "q2-an4"], correctAnswer: 2}
-]
+  {
+    question: "quest-1",
+    choices: ["q1-an1", "q1-an2", "q1-an3", "q1-an4"],
+    correctChoice: 3,
+  },
+  {
+    question: "quest-2",
+    choices: ["q2-an1", "q2-an2", "q2-an3", "q2-an4"],
+    correctChoice: 2,
+  },
+];
 
 let currentQuestion = 0;
 
-startButton.addEventListener('click', function (e) {
-  questionDiv.classList.remove('hide');
-  startScreen.classList.add('hide');
+startButton.addEventListener("click", function (e) {
+  questionDiv.classList.remove("hide");
+  startScreen.classList.add("hide");
   displayQuestion();
 });
 
@@ -22,10 +29,10 @@ function displayQuestion() {
   const curQuestion = questions[currentQuestion];
   questionTitle.textContent = curQuestion.question;
 
-  for (let i = 0; i < curQuestion.answers.length; i++) {
-    const answer = curQuestion.answers[i];
-    let button = document.createElement('button');
-    button.innerText = answer;
+  for (let i = 0; i < curQuestion.choices.length; i++) {
+    const choice = curQuestion.choices[i];
+    let button = document.createElement("button");
+    button.innerText = choice;
     choicesDiv.appendChild(button);
   }
 }

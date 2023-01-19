3ce57fcab2de145a248a5d4c61e1f3574033bb1d
WarrenT <warrentyler1024@gmail.com>
2023-01-19 17:24:51 +0000

    sort highscores based on greatest time scored

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -2,6 +2,8 @@ const highscoresEl = document.querySelector("#highscores");
 const clearButtonEl = document.querySelector("#clear");
 const highscores = JSON.parse(localStorage.getItem("highscores")) || [];
 
+highscores.sort((a, b) => b.time - a.time);
+
 highscores.forEach((score) => {
   const scoreEl = document.createElement("li");
   scoreEl.innerHTML = `${score.initials} - ${score.time}`;
